48266b442ab4206057b0c7c2c581ff5df3f443a9
Vlad Zat <vlad.zat@mydit.ie>
2018-02-21 18:52:03 +0000

    Create replica set WIP

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -5,7 +5,7 @@ const gcp = require('../../lib/cloud-platforms/gcp');
 
 const routes = express.Router();
 
-routes.post('/create/database', async (req, res) => {
+routes.post('/create/singlenode/db', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
     try {
@@ -45,6 +45,65 @@ routes.post('/create/database', async (req, res) => {
 
         if (serverInfo.cloudPlatform === 'gcp') {
             gcp.createSingleNodeDB(username, serverInfo.region, serverInfo.machineType, Number(serverInfo.diskSize), serverData);
+            res.end(JSON.stringify({'ok': 1}));
+        }
+        else {
+            throw new Error('Invalid Cloud Platform');
+        }
+    }
+    catch (err) {
+        logger.log('error', err);
+        res.end(JSON.stringify({'ok': 0, 'error': err}));
+    }
+});
+
+routes.post('/create/replicaset/db', async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    try {
+        const username = req.body.username;
+
+        if (!username) throw new Error('Invalid username');
+
+        // ServerInfo
+        // ---> cloudPlatform
+        // ---> regions[]
+        // ---> machineType
+        // ---> diskSize
+        const serverInfo = req.body.serverInfo;
+
+        if (!serverInfo ||
+            !serverInfo.cloudPlatform ||
+            !serverInfo.region ||
+            !serverInfo.machineType ||
+            !serverInfo.diskSize)
+            throw new Error('Invalid Server Info');
+
+
+        // ServerData
+        // ---> serverName
+        // ---> serverPorts[]
+        // ---> rootUser
+        // ---> rootPass
+        // ---> mongoVersion
+        const serverData = req.body.serverData;
+
+        if (!serverData ||
+            !serverData.serverName ||
+            !serverData.serverPort ||
+            !serverData.rootUser ||
+            !serverData.rootPass ||
+            !serverData.mongoVersion)
+            throw new Error('Invalid Server Data');
+
+        // Check correct no of replicas
+        if (serverInfo.regions.length === 0 ||
+            serverData.serverPorts.length === 0 ||
+            serverInfo.regions.length !== serverData.serverPorts.length)
+            throw new Error('Invalid Server Data');
+
+        if (serverInfo.cloudPlatform === 'gcp') {
+            gcp.createReplicaSetDB(username, serverInfo.region, serverInfo.machineType, Number(serverInfo.diskSize), serverData);
             res.end(JSON.stringify({'ok': 1}));
         }
         else {
