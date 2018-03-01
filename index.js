be581c5cb09ac51ade1f9f9977965aa3ffde86e7
Vlad Zat <vlad.zat@mydit.ie>
2018-03-01 17:38:20 +0000

    Add Client

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 global.Promise=require('bluebird');
 
-// const spawn = require('cross-spawn');
+const spawn = require('cross-spawn');
 
 const server = require('./server');
-// const logger = require('./lib/logger');
+const logger = require('./lib/logger');
 
 server.then( () => {
     // Node's spawn function does not work correctly on Windows
     // so cross-spawn is used to support both Linux and Windows
-    // spawn('npm', ['start'], {cwd: 'client'});
-    // logger.log('info', 'Login Manager Client running on port 3001');
+
+    if (process.env.NODE_ENV !== 'production') {
+        spawn('npm', ['start'], {cwd: 'client'});
+    }
+    logger.log('info', 'Login Manager Client running on port 4001');
 });
