16230c78b3cf8a8ec0fc36f68b52d8c698201cb1
Vlad Zat <vlad.zat@mydit.ie>
2018-03-23 11:53:36 +0000

    Table Example

--- client/src/components/InstanceOverview.js
+++ client/src/components/InstanceOverview.js
@@ -2,16 +2,82 @@ import React, { Component } from 'react';
 
 import './css/InstanceOverview.css';
 
-import { Divider } from 'semantic-ui-react';
+import { Segment, Table, Grid } from 'semantic-ui-react';
 
 class InstanceOverview extends Component {
     render() {
         return (
           <div className="InstanceOverview">
-              Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider /> Overview <Divider />
+              <Table celled singleLine>
+                  <Table.Header>
+                      <Table.Row>
+                          <Table.HeaderCell colSpan = '3'>
+                              lee2-instance1.mongostratus.me:27017
+                          </Table.HeaderCell>
+                      </Table.Row>
+                  </Table.Header>
+                  <Table.Body>
+                      <Table.Row>
+                          <Table.Cell>
+                              Virtual CPU
+                          </Table.Cell>
+                          <Table.Cell>
+                              0.2
+                          </Table.Cell>
+                      </Table.Row>
+                      <Table.Row>
+                          <Table.Cell>
+                              Memory (GB)
+                          </Table.Cell>
+                          <Table.Cell>
+                              0.60
+                          </Table.Cell>
+                      </Table.Row>
+                      <Table.Row>
+                          <Table.Cell>
+                              Disk Space (GB)
+                          </Table.Cell>
+                          <Table.Cell>
+                              10
+                          </Table.Cell>
+                      </Table.Row>
+                      <Table.Row>
+                          <Table.Cell>
+                              Region
+                          </Table.Cell>
+                          <Table.Cell>
+                              N.Virginia
+                          </Table.Cell>
+                      </Table.Row>
+                      <Table.Row>
+                          <Table.Cell>
+                              IP Address
+                          </Table.Cell>
+                          <Table.Cell>
+                              78.106.68.1
+                          </Table.Cell>
+                      </Table.Row>
+                  </Table.Body>
+              </Table>
           </div>
         );
     }
 }
 
 export default InstanceOverview;
+
+// <Segment.Group stackable doubling>
+//     <Segment raised vertical>
+//         lee2-instance1.mongostratus.me:27017
+//         <Grid celled container>
+//             <Grid.Row>
+//                 <Grid.Column>
+//                     Virtual CPU
+//                 </Grid.Column>
+//                 <Grid.Column>
+//                     0.2
+//                 </Grid.Column>
+//             </Grid.Row>
+//         </Grid>
+//     </Segment>
+// </Segment.Group>
